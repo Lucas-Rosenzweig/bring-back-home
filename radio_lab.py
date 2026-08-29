ba0f45c26b148307521c5a8052d1a9988525eb09
Lucas <xeit0w@gmail.com>
2026-08-29 20:33:53 +0200

    FrameControl modelisation

---
+++ radio_lab.py
@@ -0,0 +1,36 @@
+import socket
+
+from frame_control import FrameControl
+
+sock = socket.socket(
+    socket.AF_PACKET,
+    socket.SOCK_RAW,
+    socket.htons(0x0003),
+)
+
+sock.bind(("mon0", 0))
+
+def strip_radiotap(packet: bytes) -> bytes | None:
+
+    if len(packet) < 4:
+        return None
+
+    radiotap_length = int.from_bytes(packet[2:4],"little")
+
+    if len(packet) <= radiotap_length + 2:
+        return None
+
+    return packet[radiotap_length:]
+
+while True:
+    packet, _ = sock.recvfrom(65535)
+
+    #Strip radiotap header
+    wifi_frame = strip_radiotap(packet)
+    if not wifi_frame:
+        continue
+
+    print(int.from_bytes(wifi_frame[2:4],"little"))
+    frame_control = FrameControl(wifi_frame[0:2])
+    if(frame_control.type == 0 and frame_control.subtype == 13):
+        frame_control.print()
