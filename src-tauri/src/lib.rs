68847731383b71b1f2693af8f74eac194cab24fe
Lucas Rosenzweig <xeit0w@gmail.com>
2026-09-08 13:58:05 +0200

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,23 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
+const DATABASE_URL: &str = "sqlite:bring-back-home.db";
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(
+            tauri_plugin_sql::Builder::new()
+                .add_migrations(
+                    DATABASE_URL,
+                    vec![Migration {
+                        version: 1,
+                        description: "create pokemon table",
+                        sql: include_str!("../migrations/0001_create_pokemon.sql"),
+                        kind: MigrationKind::Up,
+                    }],
+                )
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
